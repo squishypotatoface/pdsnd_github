7353056527db1eaa914e92bbaa833d80cca443f7
Nellys Liang <nellysli@gmail.com>
2023-07-12 00:18:24 -0400

    docs: NL_bikeshare.py add line comment restart
    Add a line comment for code to allow program user to restart
    bikeshare program:  "# Display option to let user run the program again".
    This clarifies the purpose of this part of code.

--- NL_bikeshare.py
+++ NL_bikeshare.py
@@ -327,7 +327,7 @@ def main():
             else:
                 break
 
-
+        # Display option to let user run the program again
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
